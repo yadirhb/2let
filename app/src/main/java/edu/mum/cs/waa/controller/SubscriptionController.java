011486fec3b4d8ee1b2f827af355d4c443432f46
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-11-19 17:31:06 -0600

    Added base page join flow

---
+++ app/src/main/java/edu/mum/cs/waa/controller/SubscriptionController.java
@@ -0,0 +1,21 @@
+package edu.mum.cs.waa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/subscription")
+public class SubscriptionController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String joinPage() {
+        return "subscription/join";
+    }
+
+    @RequestMapping(value = "/joinForm", method = RequestMethod.GET)
+    public String joinForm(@ModelAttribute("newMember")){
+        return "subscription/form";
+    }
+}
